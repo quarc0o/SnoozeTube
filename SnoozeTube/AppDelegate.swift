bd41b15ff0cca8499e12c177340ae7e0aab8eb73
quarc0o <emmanuelq150@gmail.com>
2023-04-29 12:23:08 +0200

    Initial Commit

---
+++ SnoozeTube/AppDelegate.swift
@@ -0,0 +1,17 @@
+//Created for SnoozeTube in 2023
+// Using Swift 5.0
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
